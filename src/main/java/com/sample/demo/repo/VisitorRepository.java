531e16892b439a9ff285478b3f0fe6f5a8a5e2f2
Kennu76 <kevin.kanarbik@gmail.com>
2021-02-11 11:22:26 +0200

    created angular app

--- src/main/java/com/sample/demo/repo/VisitorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sample.demo.repo;
-
-import com.sample.demo.model.Visitor;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface VisitorRepository extends CrudRepository<Visitor, Long> {}
--- src/main/resources/db/migration/V1__create_visitor.sql
+++
@@ -1,8 +0,0 @@
-create table VISITOR (
-    id int not null AUTO_INCREMENT,
-    NAME varchar(100) not null,
-    email varchar(100) not null,
-    coming_from varchar(100) not null,
-    comments varchar(100) not null,
-    PRIMARY KEY (id)
-);
